909c124aeabab00a914fc99627108366166c14ff
Griphcode <148160412+GriphcodeDev@users.noreply.github.com>
2025-09-16 20:32:21 +0200

    Changes

---
+++ main.js
@@ -0,0 +1,23 @@
+const navbar = document.getElementById("nav");
+const MainPage = document.getElementById("main");
+const ChangeColor = document.getElementById("colorchange");
+const BackgroundColor = document.getElementById("background");
+
+
+
+
+
+ChangeColor.addEventListener("click", () => {
+        
+        console.log(BackgroundColor);
+
+        innerHTML = BackgroundColor.style.background = '#a2d2ff';
+        innerHTML = BackgroundColor.style.background = '#cdb4db'
+        
+        
+});
+
+
+
+
+
